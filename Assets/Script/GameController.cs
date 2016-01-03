efebfb1a6ccb4d1c236d09aa91a1e7753eee3751
Iskandar <handyboy.ikhank@gmail.com>
2016-01-04 00:40:25 +0700

    Set Star Pattern in Stage

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -9,6 +9,8 @@ public class GameController : MonoBehaviour {
 	public List<GameObject> ListStars = new List<GameObject> ();
 	public List<Sprite> ListSprites = new List<Sprite> ();
 	public List<int> ListStarsCount = new List<int> ();
+	public List<int> ListActvatedStars = new List<int> ();
+	public List<List<int>> ListCorrectStars = new List<List<int>> ();
 	
 	GameObject Star;
 	
@@ -25,6 +27,7 @@ public class GameController : MonoBehaviour {
 	int YLimit;
 	int CurrentStarsCount;
 	int Stage;
+	int ActivatedStars = 0;
 	
 	Vector2[,] StarPosistion;
 
@@ -41,9 +44,22 @@ public class GameController : MonoBehaviour {
 		Star = ListStars [0];
 		CurrentStar = Star.GetComponent<StarScript> ();
 		LightCurrentStar = Star.GetComponent<LineRenderer> ();
-		Stage = PlayerPrefs.GetInt("Stage");
-		
+
+		//Stage = PlayerPrefs.GetInt("Stage");
+		Stage = 1;
 		CurrentStarsCount = ListStarsCount[Stage];
+
+		List<int> dummyCombination = new List<int> ();
+		dummyCombination.Add (0);
+		ListCorrectStars.Add (dummyCombination);
+
+		List<int> firstStage = new List<int> ();
+		firstStage.Add (0);
+		firstStage.Add (1);
+		firstStage.Add (2);
+		firstStage.Add (3);
+		firstStage.Add (4);
+		ListCorrectStars.Add (firstStage);
 	}
 	
 	// Update is called once per frame
@@ -51,43 +67,51 @@ public class GameController : MonoBehaviour {
 		if (CurrentStar.isStar) {
 			if(Input.GetMouseButtonDown(0))
 			{
+				RemoveCollidersRecursively();
 				ClickPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 				StartPoint = Star.transform.position;
 				ClickPoint.z = 10;
 				EndPoint = ClickPoint;
 				LightCurrentStar.SetPosition(1, EndPoint);
-				addColliderToLine();
+				print ("Location EndPoint x : " + EndPoint.x + " Location EndPoint y : " + EndPoint.y);
+				AddColliderToLine();
 			}
 		}
 	}
 	
 	public void ActiveInitStar(int StarId){
-		CurrentStar.isStar = false;
-		Star = ListStars [StarId];
-		CurrentStar = ListStars [StarId].GetComponent<StarScript> ();
-		LightCurrentStar = ListStars[StarId].GetComponent<LineRenderer> ();
-		CurrentStar.isStar = true;
-		CurrentStar.isActive = true;
+		if (ListCorrectStars [Stage][ActivatedStars] == StarId) {
+			CurrentStar.isStar = false;
+			Star = ListStars [StarId];
+			CurrentStar = ListStars [StarId].GetComponent<StarScript> ();
+			LightCurrentStar = ListStars [StarId].GetComponent<LineRenderer> ();
+			CurrentStar.isStar = true;
+			CurrentStar.isActive = true;
+			ActivatedStars++;
+		}
 	}
 	
 	public void changeSprite(int StarId){
-		if (ListStars [StarId].GetComponent<SpriteRenderer> ().sprite == ListSprites [1]) {
-			ListStars [StarId].GetComponent<SpriteRenderer> ().sprite = ListSprites [2];
-		} else {
-			ListStars [StarId].GetComponent<SpriteRenderer> ().sprite = ListSprites [1];
+		if (ListCorrectStars [Stage] [ActivatedStars] == StarId) {
+			if (ListStars [StarId].GetComponent<SpriteRenderer> ().sprite == ListSprites [0]) {
+				StartCoroutine (SpriteLoader (StarId));
+			} else {
+				ListStars [StarId].GetComponent<SpriteRenderer> ().sprite = ListSprites [0];
+			}
 		}
 	}
 	
 	public void OnCollisionEnter(Collision col)
 	{
-		if (col.gameObject.tag == "Star") 
-		{
+		if (col.gameObject.tag == "Star") {
 			print ("collide");
+		} else {
+			print ("line dude");
 		}
 	}
 	
 	
-	private void addColliderToLine()
+	private void AddColliderToLine()
 	{
 		BoxCollider col = new GameObject("Collider").AddComponent<BoxCollider> ();
 		col.transform.parent = LightCurrentStar.transform; // Collider is added as child object of line
@@ -97,6 +121,7 @@ public class GameController : MonoBehaviour {
 		col.transform.position = midPoint; // setting position of collider object
 		
 		// Following lines calculate the angle between startPos and endPos
+		/*
 		float angle = (Mathf.Abs (StartPoint.y - EndPoint.y) / Mathf.Abs (StartPoint.x - EndPoint.x));
 		
 		if((StartPoint.y<EndPoint.y && StartPoint.x>EndPoint.x) || (EndPoint.y<StartPoint.y && EndPoint.x>StartPoint.x))
@@ -104,8 +129,25 @@ public class GameController : MonoBehaviour {
 			angle*=-1;
 		}
 		
-		angle = Mathf.Rad2Deg * Mathf.Atan (angle);
-		col.transform.Rotate (0, 0, angle);
+		angle = Mathf.Rad2Deg * Mathf.Atan (angle); 
+		col.transform.Rotate (0, 0, angle); */
+
+		print ("Location collider x : " + col.transform.position.x + " Location collider y : " + col.transform.position.y);
+		//col.enabled{};
+	}
+
+	private void RemoveCollidersRecursively() {
+		var children = new List<GameObject>();
+
+		foreach (Transform child in LightCurrentStar.transform) children.Add(child.gameObject);
+		children.ForEach (child => Destroy (child));
+	}
+
+	IEnumerator SpriteLoader (int StarId) 
+	{
+		ListStars [StarId].GetComponent<SpriteRenderer> ().sprite = ListSprites [1];
+		yield return new WaitForSeconds(1);
+		ListStars [StarId].GetComponent<SpriteRenderer> ().sprite = ListSprites [2];
 	}
 	
 }
--- Assets/Script/StarScript.cs
+++ Assets/Script/StarScript.cs
@@ -9,6 +9,8 @@ public class StarScript : MonoBehaviour {
 	public bool isActive;
 	public LineRenderer xray;
 	Vector3 startPoint;
+	Color c1 = Color.cyan;
+	Color c2 = Color.white;
 
 	// Use this for initialization
 	void Start () {
@@ -18,6 +20,8 @@ public class StarScript : MonoBehaviour {
 		xray.SetPosition (0, startPoint);
 		xray.SetPosition (1, startPoint);
 		xray.SetWidth (10, 10);
+		xray.material = new Material(Shader.Find("Particles/Additive"));
+		xray.SetColors(c1, c2);
 		isStar = false;
 	}
 	
